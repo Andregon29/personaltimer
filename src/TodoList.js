a6c1e2f4e9b44af42c1da34727f5d8dbae5aa6bb
Giorgi Maisuradze <maisuradze200329@gmail.com>
2023-05-16 16:06:37 +0400

    Design Changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,58 +1,69 @@
-import React, { useState } from 'react'
-import './TodoList.css'
+import React, { useState } from "react";
+import "./TodoList.css";
 
 function TodoList() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
 
-    const [input, setInput] = useState('')
-    const [tasks, setTasks] = useState([])
-    
-    function handleCompleted(taskid){
-        setTasks(tasks => [...tasks].map((task)=>{
-            if(taskid === task.id){
-                    return({...task, completed: !task.completed})
-            }
-            return task
-        }))
-    }
-
-    function addNewTask (){
-        if(input !== ''){
-            let id = tasks.length + 1;
-            setTasks((prev)=>[
-                ...prev,
-                {
-                    id: id,
-                    task: input,
-                    completed: false,
-                },
-            ])
-            setInput('')
+  function HandleCompleted(taskid) {
+    setTasks((tasks) =>
+      [...tasks].map((task) => {
+        if (taskid === task.id) {
+          return { ...task, completed: !task.completed };
         }
-    }
+        return task;
+      })
+    );
+  }
 
+  function AddNewTask() {
+    if (input !== "") {
+      let id = tasks.length + 1;
+      setTasks((prev) => [
+        ...prev,
+        {
+          id: id,
+          task: input,
+          completed: false,
+        },
+      ]);
+      setInput("");
+    }
+  }
 
   return (
-    <div className='todo-container'>
-        <h1>
-            TodoList
-        </h1>
-        <div className='todo-inputs'>
-            <input value={input} onInput={(e) => setInput(e.target.value)}/>
-            <button onClick={()=> addNewTask()}>Add a Task</button>
-        </div>
+    <div className="todo-container">
+      <h1>TodoList</h1>
+      <div className="todo-inputs">
+        <input
+          id="input-button"
+          value={input}
+          onInput={(e) => setInput(e.target.value)}
+        />
+        <button onClick={() => AddNewTask()}>Add a Task</button>
+      </div>
 
-        <ul>
-            {tasks.map((task)=>{
-                return(
-                    <li className={task.completed ? 'completed' : ''} id={task.id} key={task.id}>
-                        <button id='toggle-complete-btn' onClick={()=> handleCompleted(task.id)}>Toggle</button>
-                        <p id='task-text'>{task.task}</p>
-                    </li>
-                )
-            })}
-        </ul>
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li
+              className={task.completed ? "completed" : ""}
+              id={task.id}
+              key={task.id}
+            >
+              <button
+                id="toggle-complete-btn"
+                onClick={() => HandleCompleted(task.id)}
+              >
+                Toggle
+              </button>
+              <p id="task-text">{task.task}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
